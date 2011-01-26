cc7df1f33407d270780ccfaaa88dbccf5e79edd0
pigne <yoann.pigne@gmail.com>
2011-01-26 17:02:56 +0100

    modification of test program

--- test/simple-traffic-application-main.cc
+++ test/simple-traffic-application-main.cc
@@ -71,7 +71,7 @@ main(int argc, char ** argv)
   bool startSumo= true;
 
   string sumoHost="localhost";
-  string sumoPath="/opt/sumo/bin/sumo-gui";
+  string sumoPath="/opt/sumo/bin/sumo";
   cmd.AddValue("sumoConfig", "The SUMO xml config file", sumoConfig);
   cmd.AddValue("sumoHost", "Name of the machine hosting SUMO", sumoHost);
   cmd.AddValue("startTime",
